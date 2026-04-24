7fca4f37b178affa4f7907919531902ea2d08a71
pavithraleka2004 <lekapavithra@gmail.com>
2026-04-24 20:34:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  base: '/restaurant/', // ← Add this. Replace with your actual GitHub repo name
+  base: '/restaurant/', 
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
