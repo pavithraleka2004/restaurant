1ae10013515f8ec9caa2523a4eebdcb1d2726b24
pavithraleka2004 <lekapavithra@gmail.com>
2026-04-24 13:19:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {HashRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
@@ -9,6 +10,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HashRouter>
     <App />
-  </React.StrictMode>
-);
+    </HashRouter>
+  </React.StrictMode>,
+)
