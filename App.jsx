955b5b9166214aadc89d046a9e272fbc703ee99a
pavithraleka2004 <lekapavithra@gmail.com>
2026-04-25 20:03:24 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home.jsx';
 import RecipeDetail from './RecipeDetail.jsx';
-import Recipe from './Recipe.jsx';
 import Recipes from './Recipe.jsx';
 import Blog from './Blog';
 import BlogDetail from './BlogDetail';
@@ -10,16 +9,14 @@ import AboutMe from './AboutMe.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} /> 
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<BlogDetail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/recipes" element={<Recipes />} /> 
+      <Route path="/recipe/:id" element={<RecipeDetail />} />
+      <Route path="/about" element={<AboutMe />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:id" element={<BlogDetail />} />
+    </Routes>
   );
 }
 
